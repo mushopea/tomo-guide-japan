99af719581c47c59f438bf87d44bd75783511a11
Mushy <mushopea@users.noreply.github.com>
2025-04-23 00:48:54 +0900

    Update StepByStepGuide.tsx

--- src/components/process/StepByStepGuide.tsx
+++ src/components/process/StepByStepGuide.tsx
@@ -364,13 +364,16 @@ const StepByStepGuide = () => {
           </div>
           
           <div className="mt-16 py-10 bg-tomodachi-red bg-opacity-10 rounded-lg px-8">
+            
             <h1 className="text-3xl font-bold text-tomodachi-black mb-4 text-center">Tomodachi Moving Checklist</h1>
+            
             <p className="text-gray-600 mb-8 text-center">
               Download our free printable Moving to Japan checklist!
             </p>
 
             <div className="text-center"><img src="/lovable-uploads/guidep.png"></div>
-            <div className="text-center">
+            
+              <div className="text-center">
               <a
                 href="/lovable-uploads/tomodachi-moving-to-japan-printable-checklist.pdf"
                 target="_blank"
@@ -381,8 +384,11 @@ const StepByStepGuide = () => {
                 Download Checklist
               </a>
             </div>
+              
           </div>
+            
         </div>
+          
       </div>
     </section>
   );
